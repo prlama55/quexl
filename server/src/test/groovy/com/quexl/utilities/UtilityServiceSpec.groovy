7092d86642eaa2b044d1eec263beaad2d440055d
Padma <prlama@deerhold.org>
2020-06-15 21:54:05 +0545

    User and services data preparation in gson

---
+++ server/src/test/groovy/com/quexl/utilities/UtilityServiceSpec.groovy
@@ -0,0 +1,18 @@
+package com.quexl.utilities
+
+import grails.testing.services.ServiceUnitTest
+import spock.lang.Specification
+
+class UtilityServiceSpec extends Specification implements ServiceUnitTest<UtilityService>{
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
